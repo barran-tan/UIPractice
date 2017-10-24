ef1d0bf42235b819492a1e30449adf6c8538b07d
tanwei <tanwei@changingedu.com>
2017-10-24 17:09:45 +0800

    update draw4

--- exam/PracticeDraw5-master/app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice05AfterOnDrawForegroundView.java
+++ exam/PracticeDraw5-master/app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice05AfterOnDrawForegroundView.java
@@ -33,9 +33,11 @@ public class Practice05AfterOnDrawForegroundView extends AppCompatImageView {
 
         // 在 super.onDrawForeground() 的下方插入绘制代码，让绘制内容盖住前景
         // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
-        /*paint.setColor(Color.parseColor("#f44336"));
+
+        // 注意：onDrawForeground在6.0（api23）及以上才能支持，之前版本只有FrameLayout支持
+        paint.setColor(Color.parseColor("#f44336"));
         canvas.drawRect(0, 40, 200, 120, paint);
         paint.setColor(Color.WHITE);
-        canvas.drawText("New", 20, 100, paint);*/
+        canvas.drawText("New", 20, 100, paint);
     }
 }
